60321f3596c69a94befee11241d280bedae0c72f
rtusiime <rtusiime@u.rochester.edu>
2021-02-28 11:37:56 -0500

    fully functional app

--- app/src/main/java/com/rtusiime/memez/DisplayActivity.kt
+++ app/src/main/java/com/rtusiime/memez/DisplayActivity.kt
@@ -1,10 +1,10 @@
 package com.rtusiime.memez
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -16,23 +16,23 @@ class DisplayActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     var caption = 1
     var image = 1
+    var views = 0
     var captionMap : MutableMap<Int, String> = mutableMapOf<Int,String>()
-    var imageMap : MutableMap<Int, Drawable?> = mutableMapOf<Int, Drawable?>()
-
-
+    var imageMap : MutableMap<Int, Drawable?> =mutableMapOf<Int,Drawable?>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+        views++
 
-        captionMap  = mutableMapOf<Int,String>(1 to getString(R.string.meme_message_1),
+        captionMap = mutableMapOf<Int,String>(1 to getString(R.string.meme_message_1),
             2 to getString(R.string.meme_message_2),3 to getString(R.string.meme_message_3),4 to getString(
                 R.string.meme_message_4),
-            5 to getString(R.string.meme_message_5), 6 to getString(R.string.meme_message_6), 7 to getString(
+            5 to resources.getString(R.string.meme_message_5), 6 to getString(R.string.meme_message_6), 7 to getString(
                 R.string.meme_message_7),
             8 to getString(R.string.meme_message_1))
 
-        imageMap  = mutableMapOf<Int, Drawable?>(
+        imageMap = mutableMapOf<Int, Drawable?>(
             1 to getDrawable(R.drawable.blinkingguymeme),
             2 to getDrawable(R.drawable.cryingjordanmeme),
             3 to getDrawable(R.drawable.bernie),
@@ -42,10 +42,8 @@ class DisplayActivity : AppCompatActivity() {
             7 to getDrawable(R.drawable.spidermanmeme),
             8 to getDrawable(R.drawable.cryingkidmeme))
 
-
-
         imageView = findViewById(R.id.imageView)
-//        image.visibility = View.VISIBLE
+
 
          textView= findViewById(R.id.textView)
 
@@ -53,14 +51,26 @@ class DisplayActivity : AppCompatActivity() {
         if (bundle!=null) {
             caption = bundle.getInt(CAPTION)
             image = bundle.getInt(IMAGE)
-//            Toast.makeText(this,msg, Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"nyokoo", Toast.LENGTH_LONG).show()
         }
         imageView.setImageDrawable(imageMap.getValue(image))
         textView.text = captionMap.getValue(caption)
+
+        val intents = Intent()
+        intent.putExtra(VIEW_NUMBER,views)
+        setResult(Activity.RESULT_OK,intents)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(VIEW_NUMBER,views)
 
+        super.onSaveInstanceState(outState)
+    }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        views = savedInstanceState.getInt(VIEW_NUMBER)
 
+    }
 
 }
--- app/src/main/java/com/rtusiime/memez/MainActivity.kt
+++ app/src/main/java/com/rtusiime/memez/MainActivity.kt
@@ -3,13 +3,14 @@ package com.rtusiime.memez
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Pair
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import com.rtusiime.memez.model.MemeCombinator
 import kotlin.random.Random
 
-const val   MEME_CODES= "ShowMemes"
 const val   REQUEST_CODE_MEMES= 0
 const val IMAGE = "image"
 const val CAPTION = "caption"
@@ -17,6 +18,8 @@ const val CAPTION = "caption"
 class MainActivity : AppCompatActivity() {
     private lateinit var generateMemeButton: ImageButton
     private val combinator = MemeCombinator()
+    var image =1
+    var caption =1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +29,22 @@ class MainActivity : AppCompatActivity() {
 
         generateMemeButton = findViewById(R.id.generate_meme_button)
         generateMemeButton.setOnClickListener{
+            initializePairs()
             onShowOtherView()
+
+            //updateUI
+
         }
     }
+
+    private fun initializePairs(){
+        val pair = combinator.getPair()
+        caption = pair.first
+        image = pair.second
+    }
+
+
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -37,20 +53,16 @@ class MainActivity : AppCompatActivity() {
         }
         if(requestCode== REQUEST_CODE_MEMES){
 
-//            val msg = when(data?.getIntExtra(VIEW_NUMBER,0) ?: 0){
-//                1 -> getString(R.string.thanks_for_cookies)
-//                2 -> getString(R.string.thanks_for_milk)
-//                else -> getString(R.string.thanks_for_nothing)
-//            }
+
            Toast.makeText(this,"request ok", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun onShowOtherView() {
         val intent = Intent(this, DisplayActivity::class.java)
-        startActivity(intent)
-        intent.putExtra(CAPTION,combinator.getPair().first)
-        intent.putExtra(IMAGE,combinator.getPair().second)
+
+        intent.putExtra(CAPTION,caption)
+        intent.putExtra(IMAGE,image)
         startActivityForResult(intent, REQUEST_CODE_MEMES)
     }
 
